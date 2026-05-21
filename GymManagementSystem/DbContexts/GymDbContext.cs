3beef0ba6562c22f724f6ca50365338fd58c7f06
karimezz22 <74823379+karimezz22@users.noreply.github.com>
2026-05-21 15:20:20 +0300

    feat(database): add EF Core setup and GymDbContext

---
+++ GymManagementSystem/DbContexts/GymDbContext.cs
@@ -0,0 +1,21 @@
+﻿using GymManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace GymManagementSystem.DbContexts
+{
+    public class GymDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = GymManagementSystemDb; Trusted_Connection = true; TrustServerCertificate = true");
+        }
+
+        public DbSet<Plan> Plans => Set<Plan>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
