38af7aadb5aa4c07ab4d70cfd7db6f7d14e12da2
karimezz22 <74823379+karimezz22@users.noreply.github.com>
2026-05-21 16:20:22 +0300

    feat(plan): add Plan module

---
+++ GymManagementSystem/Configurations/PlanConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using GymManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GymManagementSystem.Configurations
+{
+    public class PlanConfiguration : IEntityTypeConfiguration<Plan>
+    {
+        public void Configure(EntityTypeBuilder<Plan> builder)
+        {
+            builder.ToTable(p =>
+            {
+                p.HasCheckConstraint("PlanDurationCheck", "DurationDays Between 1 and 365");
+            });
+
+            builder.Property(p => p.Name)
+                   .HasColumnType("varchar")
+                   .HasMaxLength(50); 
+
+            builder.Property(p => p.Description)
+                   .HasMaxLength(200);
+
+            builder.Property(p => p.Price)
+                   .HasPrecision(10, 2);
+
+            builder.Property(p => p.CreatedAt)
+                   .HasDefaultValueSql("GETDATE()");
+        }
+    }
+}
---
+++ GymManagementSystem/Controllers/PlansController.cs
@@ -0,0 +1,40 @@
+﻿using GymManagementSystem.DbContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagementSystem.Controllers
+{
+    public class PlansController : Controller
+    {
+        private readonly GymDbContext dbContext;
+
+        public PlansController()
+        {
+            dbContext = new GymDbContext();
+        }
+
+        //Index Action
+        // Get BaseURL/Plans/Index Or BaseURL/Plans => Listing All Plans 
+        public async Task<IActionResult> Index()
+        {
+            var plans = await dbContext.Plans.ToListAsync();
+
+            return View(plans);
+        }
+
+        //Details Action
+        // Get BaseURL/Plans/Details/ID => Displaying Details of a Specific Plan
+        public async Task<IActionResult> Details(int id)
+        {
+            var plan = await dbContext.Plans.FindAsync(id);
+
+            if (plan is null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(plan);
+        }
+
+    }
+}
