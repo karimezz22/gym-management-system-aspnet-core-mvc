83741d25e280f712dcc2acf332e6284b52bec9a9
karimezz22 <74823379+karimezz22@users.noreply.github.com>
2026-05-24 20:58:33 +0300

    refactor(plan): introduce repository pattern

---
+++ GymManagement.DAL/Repositories/Classes/PlanRepository.cs
@@ -0,0 +1,50 @@
+﻿using GymManagement.DAL.DbContexts;
+using GymManagement.DAL.Models;
+using GymManagement.DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagement.DAL.Repositories.Classes
+{
+    public class PlanRepository : IPlanRepository
+    {
+        private readonly GymDbContext dbContext;
+
+        public PlanRepository()
+        {
+            dbContext = new GymDbContext();
+        }
+
+        public async Task<int> AddAsync(Plan plan, CancellationToken cancellationToken = default)
+        {
+            dbContext.Plans.Add(plan);
+
+            return await dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<int> DeleteAsync(Plan plan, CancellationToken cancellationToken = default)
+        {
+            dbContext.Plans.Remove(plan);
+
+            return await dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<IEnumerable<Plan>> GetAllAsync(bool tracking = false, CancellationToken cancellationToken = default)
+        {
+            IQueryable<Plan> query = tracking ? dbContext.Plans : dbContext.Plans.AsNoTracking();
+
+            return await query.ToListAsync(cancellationToken);
+        }
+
+        public async Task<Plan?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await dbContext.Plans.FindAsync(new object[] { id }, cancellationToken);
+        }
+
+        public async Task<int> UpdateAsync(Plan plan, CancellationToken cancellationToken = default)
+        {
+            dbContext.Plans.Update(plan);
+
+            return await dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ GymManagement.DAL/Repositories/Interfaces/IPlanRepository.cs
@@ -0,0 +1,13 @@
+﻿using GymManagement.DAL.Models;
+
+namespace GymManagement.DAL.Repositories.Interfaces
+{
+    public interface IPlanRepository
+    {
+        Task<IEnumerable<Plan>> GetAllAsync(bool tracking = false, CancellationToken cancellationToken = default);
+        Task<Plan?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<int> AddAsync(Plan plan, CancellationToken cancellationToken = default);
+        Task<int> UpdateAsync(Plan plan, CancellationToken cancellationToken = default);
+        Task<int> DeleteAsync(Plan plan, CancellationToken cancellationToken = default);
+    }
+}
